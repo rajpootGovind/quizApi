367f96b83787063b36c6864d5abbf6b405655c5b
rajpootGovind <govindrajpoot12000@gmail.com>
2024-10-13 09:43:25 +0530

    Completed from my side

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -2,8 +2,8 @@ const quizController = require("../controller/quiz.controller")
 const MW = require("../middleware/creator.mw")
  
 module.exports = (app)=>{
-     app.post("/quiz/api/", quizController.createQuiz)
+     app.post("/quiz/api/",[MW.verifyToken, MW.isAdmin], quizController.createQuiz)
      app.get("/quiz/api/", quizController.getQuizzes)
      app.get("/quiz/api/:id", quizController.getQuizDetails)
-     app.post("/quiz/api/:id/result",quizController.result)
+     app.post("/quiz/api/:id/result",[MW.verifyToken, MW.isAdmin],quizController.result)
 }
